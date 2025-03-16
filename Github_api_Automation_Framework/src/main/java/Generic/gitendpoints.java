54535bf4cc015cf123aab4576855561ce2d3a01c
BrahmendraJayaraju <81387382+BrahmendraJayaraju@users.noreply.github.com>
2025-03-15 22:34:12 -0400

    jjgj

--- Github_api_Automation_Framework/src/main/java/Generic/gitendpoints.java
+++ Github_api_Automation_Framework/src/main/java/Generic/gitendpoints.java
@@ -5,5 +5,6 @@ public interface gitendpoints {
 	String getRepo = "/repos/{owner}/{repo}";
 	String postoRepo = "/user/repos";
 	String updateRpo = "/repos/{owner}/{repo}";
+	
 	String deleteRepo = "/repos/{owner}/{repo}";
 }
